f195d3950726bfe255cbee98e694bae447cc7593
user <@>
2026-06-04 18:23:24 +0300

    create relationships between user and post

--- core/models/post.py
+++ core/models/post.py
@@ -1,8 +1,13 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import String, Text, ForeignKey
 
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+
+if TYPE_CHECKING:
+    from .user import User
 
 
 class Post(Base):
@@ -16,3 +21,5 @@ class Post(Base):
     user_id: Mapped[int] = mapped_column(
         ForeignKey("user.id"),
     )
+
+    user: Mapped["User"] = relationship(back_populates="posts")
